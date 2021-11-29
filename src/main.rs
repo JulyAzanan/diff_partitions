a6bf72d5230ac9cfb1b3d1fd6d4d9aec4aab85aa
JulyAzanan <samuel.benhaim.lyc@gmail.com>
2021-11-29 23:50:36 +0100

    working xml parsing convert to a correct object

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+/* /* #[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_xml_rs;
+
+pub mod types;
+use types::ScorePartwise; */
+
+extern crate minidom;
+
+use std::fs::File;
+use std::io::Read;
+use std::fs;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    // let mut file = File::open("./tests/1AjoutSameMesure_o.musicxml").unwrap();
+    let mut file = File::open("./tests/test.xml").unwrap();
+    // let file = BufReader::new(file);
+    let mut s = String::new();
+
+    file.read_to_string(&mut s).expect("Nani?!");
+    println!("{}", s);
+    let root: minidom::Element = s.parse().expect("Fatchouna");
+    println!("{:#?}", root);
+}
+ */
+
+use std::io::Read;
+use std::fs;
+use std::io::BufReader;
+
+pub mod types;
+
+extern crate xmltree;
+
+use xmltree::Element;
+use std::fs::File;
+
+fn main() {
+    let mut file = File::open("./tests/1AjoutSameMesure_o.musicxml").unwrap();
+    let mut s = String::new();
+
+    file.read_to_string(&mut s).expect("nani?");
+    /* let data: &'static str = r##"
+    <?xml version="1.0" encoding="utf-8" standalone="yes"?>
+    <names>
+        <name first="bob" last="jones" />
+        <name first="elizabeth" last="smith" />
+    </names>
+    "##; */
+    
+    let names_element = Element::parse(s.as_bytes()).unwrap();
+
+    let mut parts: Vec<types::Part> = Vec::new();
+    for child in names_element.children.iter() {
+        let child = child.as_element().expect("Ce n'est pas une node ;-;");
+        if child.name == "part" {
+            parts.push(types::parsed_to_part(child))
+        }
+    }
+
+    
+    println!("{:#?}", parts);
 }
---
+++ src/types.rs
@@ -0,0 +1,198 @@
+#[derive(Debug)]
+pub struct Pitch {
+    step: char,
+    octave: u8,
+}
+
+#[derive(Debug)]
+pub struct Note {
+    pitch: Option<Pitch>, //Pitch(Step, octave, duration, type)
+    duration: u8, //Silence(duration, type)
+    typee: String,
+}
+
+#[derive(Debug)]
+pub struct Clef {
+    sign: char,
+}
+
+#[derive(Debug)]
+pub struct Part {
+    measures: Vec<Mesure>,
+}
+
+#[derive(Debug)]
+pub struct ScorePartwise {
+    part: Part,
+}
+
+#[derive(Debug)]
+pub struct Attributes {
+    divisions: u8, //unité de division de la noire le plus petit apparaissant dans la partition entière (1 pour une partition avec que des noires, 2 pour une contenant aussi des croches, etc)
+    clef: Clef,    //Clé de la portée
+    time: Time,
+}
+
+#[derive(Debug)]
+pub struct Time {
+    beats: u8, //nombre du haut dans l'indication de mesure
+    beat_type: u8, //nombre du bas dans l'indication de mesure
+}
+
+#[derive(Debug)]
+pub struct Mesure {
+    attributes: Option<Attributes>,
+    notes: Vec<Note>, //Ensemble des notes et silences contenus dans la mesure
+}
+
+impl Mesure {
+    pub fn print_mesure(&self) -> () {
+        println!("Mesure : {:?}", self);
+        for note in self.notes.iter() {
+            println!("{:?}", note)
+        }
+    }
+}
+
+impl ScorePartwise {
+    pub fn print_score(&self) -> () {
+        println!("Score : {:?}", self);
+        for measure in self.part.measures.iter() {
+            measure.print_mesure();
+        }
+    }
+}
+
+pub fn parsed_to_part(parsed: &xmltree::Element) -> Part {
+    let mut res = Part {measures: Vec::new()};
+    for measure in parsed.children.iter() {
+        let measure = measure.as_element().expect("Ce n'est pas une mesure ;-;");
+        res.measures.push(parsed_to_measure(measure))
+    }
+    res
+}
+
+pub fn parsed_to_measure(parsed: &xmltree::Element) -> Mesure {
+    let mut res = Mesure {
+        attributes: None,
+        notes: Vec::new()
+    };
+    for child in parsed.children.iter() {
+        let child = child.as_element().expect("Ce n'est pas une node ;-;");
+        if child.name == "attributes" {
+            res.attributes = Some(parsed_to_attributes(child))
+        }
+        if child.name == "note" {
+            res.notes.push(parsed_to_note(child))
+        }
+    }
+    res
+}
+
+pub fn parsed_to_attributes(parsed: &xmltree::Element) -> Attributes {
+    let clef = Clef {
+        sign: 'a'
+    };
+    let time = Time {
+        beats: 0,
+        beat_type: 0
+    };
+    let mut res = Attributes {
+        divisions: 0,
+        clef: clef,
+        time: time
+    };
+    for child in parsed.children.iter() {
+        let child = child.as_element().expect("Ce n'est pas une node ;-;");
+        if child.name == "divisions" {
+            res.divisions = string_to_u8(child.children[0].as_text().expect("C'est pas un text wesh"));
+        }
+        if child.name == "time" {
+            res.time = parsed_to_time(child)
+        }
+        if child.name == "clef" {
+            res.clef = parsed_to_clef(child)
+        }
+    }
+    res
+}
+
+pub fn parsed_to_clef(parsed: &xmltree::Element) -> Clef {
+    let mut res = Clef {
+        sign: 'a'
+    };
+    for child in parsed.children.iter() {
+        let child = child.as_element().expect("Ce n'est pas une node ;-;");
+        if child.name == "sign" {
+            res.sign = child.children[0].as_text().expect("C'est pas un text wesh").chars().nth(0).expect("String vide");
+        }
+    }
+    res
+}
+
+pub fn parsed_to_time(parsed: &xmltree::Element) -> Time {
+    let mut res = Time {
+        beats: 0,
+        beat_type: 0
+    };
+    for child in parsed.children.iter() {
+        let child = child.as_element().expect("Ce n'est pas une node ;-;");
+        if child.name == "beats" {
+            res.beats = string_to_u8(child.children[0].as_text().expect("C'est pas un text wesh"))
+        }
+        if child.name == "beat-type" {
+            res.beat_type = string_to_u8(child.children[0].as_text().expect("C'est pas un text wesh"))
+        }
+    }
+    res
+}
+
+pub fn parsed_to_note(parsed: &xmltree::Element) -> Note {
+    let mut res = Note {
+        duration: 0,
+        typee: String::new(),
+        pitch: None
+    };
+    let mut is_silence = false;
+    for child in parsed.children.iter() {
+        let child = child.as_element().expect("Ce n'est pas une node ;-;");
+        if child.name == "duration" {
+            res.duration = string_to_u8(child.children[0].as_text().expect("C'est pas un text wesh"))
+        }
+        if child.name == "type" {
+            res.typee = child.children[0].as_text().expect("C'est pas un text wesh").to_string();
+        }
+        if child.name == "rest" {
+            is_silence = true;
+        }
+        if child.name == "pitch" {
+            res.pitch = if is_silence {
+                None
+            } else {
+                Some(parsed_to_pitch(child))
+            }
+        }
+    }
+    res
+}
+
+pub fn parsed_to_pitch(parsed: &xmltree::Element) -> Pitch {
+    let mut res = Pitch {
+        octave: 0,
+        step: 'a'
+    };
+    for child in parsed.children.iter() {
+        let child = child.as_element().expect("Ce n'est pas une node ;-;");
+        if child.name == "octave" {
+            res.octave = string_to_u8(child.children[0].as_text().expect("C'est pas un text wesh"))
+        }
+        if child.name == "step" {
+            res.step = child.children[0].as_text().expect("C'est pas un text wesh").chars().nth(0).expect("string vide");
+        }
+    }
+    res
+}
+
+fn string_to_u8(s: &str) -> u8 {
+    s.as_bytes()[0] - 48
+}
